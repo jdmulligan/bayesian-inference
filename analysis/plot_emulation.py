382cdc44b5517c9293d7736086f830e1274b0d0a
James Mulligan <james.mulligan@berkeley.edu>
2023-03-03 18:30:18 -0800

    plot multiple panels of observables

--- analysis/plot_emulation.py
+++ analysis/plot_emulation.py
@@ -125,24 +125,32 @@ def _plot_pca_reconstruction_observables(results, config, plot_dir):
     Y_dict_truncated_reconstructed = data_IO.prediction_dict_from_matrix(Y_truncated_reconstructed, observables)
 
     #-----
-    # Loop through observables and plot
+    # Loop through observables and plot 
     # TODO: probably want to put the main pieces of this functionality in plot_base.py or something
 
-    for observable_label in sorted_observable_list:
+    # Group observables into subplots, with shapes specified in config
+    plot_panel_shapes = config.analysis_config['plot_panel_shapes']
+    n_panels = sum(x[0]*x[1] for x in plot_panel_shapes)
+    assert len(sorted_observable_list) < n_panels, f'You specified {n_panels} panels, but have {len(sorted_observable_list)} observables'
+    i_plot = 0
+    i_subplot = 0
+    fig, axs = None, None
+
+    for i_observable,observable_label in enumerate(sorted_observable_list):
         sqrts, system, observable_type, observable, subobserable, centrality = data_IO.observable_label_to_keys(observable_label)
         
         # Get JETSCAPE-analysis config block for that observable
         plot_config_file = os.path.join(plot_config_dir, f'STAT_{sqrts}.yaml')
         with open(plot_config_file, 'r') as stream:
             plot_config = yaml.safe_load(stream)
-            plot_block = plot_config[observable_type][observable]
+        plot_block = plot_config[observable_type][observable]
         xtitle = _latex_from_tlatex(plot_block['xtitle'])
         ytitle = _latex_from_tlatex(plot_block['ytitle_AA'])
 
         color_data = sns.xkcd_rgb['almost black']
         color_prediction = sns.xkcd_rgb['dark sky blue']
         color_prediction_reconstructed = sns.xkcd_rgb['denim blue']  # sns.xkcd_rgb['light blue'], sns.xkcd_rgb['pale red']
-        
+
         linewidth = 2
         alpha = 0.7
 
@@ -165,36 +173,51 @@ def _plot_pca_reconstruction_observables(results, config, plot_dir):
         # Get PCA-reconstructed prediction
         prediction_y_truncated_reconstruated = Y_dict_truncated_reconstructed[observable_label][design_point_index]
 
-        # Plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xlabel(rf'{xtitle}', fontsize=20)
-        ax.set_ylabel(rf'{ytitle}', fontsize=20)
-        ax.set_ylim([0., 2.])
-        ax.tick_params(labelsize=15)
-        ax.set_xlim(xmin[0], xmax[-1])
+        # Plot -- create new plot and/or fill appropriate subplot
+        plot_shape = plot_panel_shapes[i_plot]
+        fontsize = 14./plot_shape[0]
+        markersize = 8./plot_shape[0]
+        if i_subplot == 0:
+            fig, axs = plt.subplots(plot_shape[0], plot_shape[1], constrained_layout=True)
+            for ax in axs.flat:
+                ax.tick_params(labelsize=fontsize)
+            row = 0
+            col = 0
+        else:
+            col = i_subplot // plot_shape[0]
+            row = i_subplot % plot_shape[0]
+
+        axs[row,col].set_xlabel(rf'{xtitle}', fontsize=fontsize)
+        axs[row,col].set_ylabel(rf'{ytitle}', fontsize=fontsize)
+        axs[row,col].set_ylim([0., 2.])
+        axs[row,col].set_xlim(xmin[0], xmax[-1])
 
         # Draw data and predictions
-        ax.errorbar(x, data_y, xerr=xerr, yerr=data_y_err,
-                      color=color_data, marker='s', markersize=8, linestyle='', label='Experimental data')
+        axs[row,col].errorbar(x, data_y, xerr=xerr, yerr=data_y_err,
+                              color=color_data, marker='s', markersize=markersize, linestyle='', label='Experimental data')
         
-        ax.plot(x, prediction_y, label=r'JETSCAPE',
-                color=color_prediction, linewidth=linewidth, alpha=alpha)
+        axs[row,col].plot(x, prediction_y, label=r'JETSCAPE',
+                          color=color_prediction, linewidth=linewidth, alpha=alpha)
         
-        ax.plot(x, prediction_y_truncated_reconstruated, label=r'JETSCAPE (reconstructed)',
-                color=color_prediction_reconstructed, linewidth=linewidth, alpha=alpha)
+        axs[row,col].plot(x, prediction_y_truncated_reconstruated, label=r'JETSCAPE (reconstructed)',
+                          color=color_prediction_reconstructed, linewidth=linewidth, alpha=alpha)
 
         # Draw dashed line at RAA=1
-        ax.plot([xmin[0], xmax[-1]], [1, 1],
-                sns.xkcd_rgb['almost black'], alpha=alpha, linewidth=linewidth, linestyle='dotted')
+        axs[row,col].plot([xmin[0], xmax[-1]], [1, 1],
+                          sns.xkcd_rgb['almost black'], alpha=alpha, linewidth=linewidth, linestyle='dotted')
 
         # Draw legend
-        ax.legend(loc='best', title=observable_label, title_fontsize=14, fontsize=14, frameon=False)
+        axs[row,col].legend(loc='upper right', title=observable_label, 
+                            title_fontsize=fontsize, fontsize=fontsize, frameon=True)
 
-        # Save figure
-        plt.tight_layout()
-        plt.savefig(os.path.join(plot_dir, f'PCA_{observable_label}.pdf'))
-        plt.close()
+        # Increment subplot, and save if done with plot
+        i_subplot += 1
+        if i_subplot == plot_shape[0]*plot_shape[1] or i_observable == len(sorted_observable_list)-1:
+            i_plot += 1
+            i_subplot = 0
+            
+            plt.savefig(os.path.join(plot_dir, f'PCA_{i_plot}.pdf'))
+            plt.close()   
 
 #-------------------------------------------------------------------------------------------
 def _latex_from_tlatex(s):
