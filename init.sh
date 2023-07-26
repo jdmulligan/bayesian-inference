a2ff5ea3f5a1bf176cad01c169277b94da4d7393
James Mulligan <james.mulligan@berkeley.edu>
2023-07-26 10:45:13 -0700

    remove gpytorch dependency (for install simplicity) and update init.sh to support hiccupgpu

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# Set up pyenv (for python version management)
-export PYENV_ROOT="/home/software/users/james/pyenv"
-export PYTHON_CONFIGURE_OPTS="--enable-shared"
-export PATH="${PATH}:${PYENV_ROOT}/bin"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-pyenv local 3.9.12
+# On hiccup CPU, we use pyenv -- on hiccup GPU, we will use system python
+if ! lspci | grep -i 'nvidia' > /dev/null; then
+    # Set up pyenv (for python version management)
+    export PYENV_ROOT="/home/software/users/james/pyenv"
+    export PYTHON_CONFIGURE_OPTS="--enable-shared"
+    export PATH="${PATH}:${PYENV_ROOT}/bin"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    pyenv local 3.9.12
+fi
 
 # Get command line option to determine whether we need to install the virtual environment, or just enter it
 for i in "$@"; do
@@ -23,17 +26,26 @@ for i in "$@"; do
       ;;
   esac
 done
+
+# Install separate venv for GPU and CPU (to allow to work on either machine, using same home directory)
 if [ ! -z ${INSTALL} ]; then
-    if [ -d ".venv" ]; then
+    if lspci | grep -i 'nvidia' > /dev/null; then
+        VENV_DIR=".venv_gpu"
+    else
+        VENV_DIR=".venv_cpu"
+    fi
+
+    if [ -d $VENV_DIR ]; then
         echo
         echo "Remove existing virtual environment..."
-        rm -r .venv
+        rm -r $VENV_DIR
     fi
     echo
     echo "Create new virtual environment..."
-    python -m venv .venv
+    python -m venv $VENV_DIR
     pdm install
 fi
 
 # Initialize python virtual environment for package management
-source .venv/bin/activate
+source $VENV_DIR/bin/activate
+
